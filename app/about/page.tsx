b0ee8f2dfbbcecbb67db8d02b282224d365bb756
Ellaine Gonzales <ellainegonzales003@gmail.com>
2026-06-04 21:34:48 +0800

    refactor: redesign About page with responsive image-based layout
    replace custom image components with responsive next/image
    remove legacy image components and outdated assets
    add new portfolio images and updated resume PDF
    redesign About page with a mobile-first, two-column layout
    update content and resume link integration

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,70 +1,62 @@
 "use client";
 import React from "react";
-import StackImages from "./stackImages";
-import StickerPeel from "@/components/StickerPeel";
-
+import Image from "next/image";
 import { alata } from "../fonts";
 
 export default function AboutPage({ id }: { id?: string }) {
   return (
-    <>
-      <section className="mt-45 w-full h-screen" id={id}>
-        <div className="gap-15 grid grid-cols-[40%_1fr] mx-15 p-2 h-full">
-          <StickerPeel
-            imageSrc="./wow.gif"
-            width={220}
-            rotate={0}
-            peelBackHoverPct={30}
-            peelBackActivePct={40}
-            shadowIntensity={0.5}
-            lightingIntensity={0.1}
-            initialPosition={{ x: 480, y: 60 }}
-            peelDirection={0}
+    <section
+      className="relative px-6 sm:px-10 lg:px-16 py-20 w-full min-h-screen"
+      id={id}
+    >
+      <div className="items-center gap-10 lg:gap-20 grid grid-cols-1 lg:grid-cols-[1fr_1fr] mx-auto max-w-7xl h-full">
+        {/* Stack of photos */}
+        <div className="flex justify-center items-center">
+          <Image
+            src="/myself.png"
+            width={500}
+            height={500}
+            alt="about_me"
             className=""
           />
-          <StickerPeel
-            imageSrc="./flower.gif"
-            width={130}
-            rotate={0}
-            peelBackHoverPct={30}
-            peelBackActivePct={40}
-            shadowIntensity={0.5}
-            lightingIntensity={0.1}
-            initialPosition={{ x: 200, y: 500 }}
-            peelDirection={0}
-          />
+        </div>
 
-          <div className="flex justify-center items-center h-7/11">
-            <StackImages />
-          </div>
-          <div className="flex justify-center items-center">
-            <div className="p-15 text-justify">
-              <h3 className={`${alata.className} text-7xl mb-10`}>
-                It’s Me! Hi!
-              </h3>
-              <p className="mb-6 pr-20 text-2xl">
-                I’m Ellaine Gonzales, a{" "}
-                <span className="text-[var(--yellow-font)]">
-                  web developer & designer
-                </span>
-                ! I specialize in Frontend Development, focusing on building
-                high quality web experiences through clean code and thoughtful
-                design.{" "}
-              </p>
-              <div className="flex [&>a]:flex gap-2 [&>a]:gap-3 [&>a]:bg-[var(--white-background)] [&>a]:hover:bg-[var(--black-font)] [&>a]:hover:shadow-2xl [&>a]:p-2.5 [&>a]:px-7 [&>a]:border [&>a]:rounded-xl [&>a>img]:w-5 [&>a]:hover:text-[var(--yellow-font)] [&>a]:text-[var(--black-font)] [&>a]:duration-300 [&>a]:ease-in [&>a]:cursor-pointer">
-                <a href="#">
-                  <img src="./resume.svg" />
-                  Resume
-                </a>
-                <a href="https://github.com/Gnzlla">
-                  <img src="./yellow-github.svg" />
-                  Github
-                </a>
-              </div>
+        {/* Text block */}
+        <div className="flex justify-center items-center">
+          <div className="max-w-xl text-justify">
+            <h3
+              className={`${alata.className} leading-tight mb-6
+                text-5xl sm:text-6xl lg:text-7xl `}
+            >
+              Hi! It's me! 👋
+            </h3>
+
+            <p className="mb-8 text-lg sm:text-xl lg:text-2xl leading-relaxed">
+              I'm Ellaine Gonzales, a{" "}
+              <span className="text-[#E1B145]">
+                frontend developer and designer
+              </span>{" "}
+              who loves turning ideas into engaging digital experiences through
+              clean code, creative design, and seamless interactions.
+            </p>
+
+            <div className="flex [&>a]:flex flex-wrap [&>a]:items-center gap-3 [&>a]:gap-3 [&>a]:bg-[var(--white-background)] [&>a]:hover:bg-[var(--black-font)] [&>a]:hover:shadow-2xl [&>a]:p-2.5 [&>a]:px-7 [&>a]:border [&>a]:rounded-xl [&>a>img]:w-5 [&>a]:hover:text-[var(--yellow-font)] [&>a]:text-[var(--black-font)] text-sm sm:text-base [&>a]:duration-300 [&>a]:ease-in [&>a]:cursor-pointer">
+              <a
+                href="/GonzalesEllaine_Resume.pdf"
+                target="Resume"
+                rel="Resume"
+              >
+                <img src="./resume.svg" alt="Resume" />
+                Resume
+              </a>
+              <a href="https://github.com/Gnzlla">
+                <img src="./yellow-github.svg" alt="GitHub" />
+                Github
+              </a>
             </div>
           </div>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 }
